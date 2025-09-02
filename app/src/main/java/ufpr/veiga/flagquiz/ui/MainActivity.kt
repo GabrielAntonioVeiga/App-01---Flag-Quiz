9338e5ee8b05ee2e2175e8940a8b45723a6dd29c
Vitor <vitor.espinozadg@gmail.com>
2025-09-02 19:54:24 -0300

    feat: adiciona tela de resultados

--- app/src/main/java/ufpr/veiga/flagquiz/ui/MainActivity.kt
+++ app/src/main/java/ufpr/veiga/flagquiz/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package ufpr.veiga.flagquiz
+package ufpr.veiga.flagquiz.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ufpr.veiga.flagquiz.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
