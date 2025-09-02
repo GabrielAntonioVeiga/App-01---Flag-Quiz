6acc7eba4350148511800f79a694a34aca4759c9
GabrielAntonioVeiga <gabrielveiga54@gmail.com>
2025-09-02 19:22:45 -0300

    cria tela de boas vindas

--- app/src/main/java/ufpr/veiga/flagquiz/MainActivity.kt
+++ app/src/main/java/ufpr/veiga/flagquiz/MainActivity.kt
@@ -1,6 +1,10 @@
 package ufpr.veiga.flagquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun comecar(view: View) {
+        val intent = Intent(this, quiz::class.java)
+
+        val editText = findViewById<EditText>(R.id.nomeTextView)
+
+        if (editText.text.isEmpty()) {
+            Toast.makeText(this, "Por favor, insira seu nome", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        intent.putExtra("userName", editText.text.toString())
+
+        startActivity(intent)
     }
 }
